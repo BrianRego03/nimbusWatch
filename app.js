8328df050e7d7d451eb059cbc6572267fa668f45
brianrego03 <brian.rego03@gmail.com>
2025-08-07 11:59:47 +0530

    Change request origin

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    // origin: "http://localhost:3000",
-    origin:true,
+    origin: "http://localhost:5173",
+    // origin:true,
     credentials:true
 }))
 
