093fb15c28961f550ac52208dec148de1fb857e0
brianrego03 <brian.rego03@gmail.com>
2025-08-13 18:09:22 +0530

    Remove bloat and clean up json of report

--- controller/laundryReportController.js
+++ controller/laundryReportController.js
@@ -3,7 +3,6 @@ const { fetchLaundry, fetchAllWindows } = require("../db/query");
 const laundryReporter=async(req,res)=>{
     const laundryData =await fetchLaundry(+(req.params.id));
     const laundryWindows =await fetchAllWindows("laundryId",+(req.params.id));
-    // console.log(laundryWindows);
     const windowCopy = [ ...laundryWindows ];
     const weatherData = await fetch(
         "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
@@ -12,83 +11,26 @@ const laundryReporter=async(req,res)=>{
         { mode: "cors" }
     );
     const weatherJson = await weatherData.json();
-    const weatherArr = weatherDataFilter2(weatherJson,windowCopy);
+    const weatherArr = weatherDataFilter(weatherJson,windowCopy);
 
     const jsonObjector={
         laundry:laundryData,
-        windows:laundryWindows,
         weather:weatherArr,
-        weatherJson:weatherJson
+        location:weatherJson.resolvedAddress,
+        description:weatherJson.description
     }
     res.json(jsonObjector);
 }
 
+
+
 const weatherDataFilter=(weatherObj,windowArr)=>{
-    let weatherArray=[];
-    let windowDaysArr=windowArr.map((item,index)=>{
-        return item.startWindowDay;
-    })
-
-    
-
-    weatherObj.days.slice(0, 7).forEach((day, dayIndex) => {
-        let dayObj = new Date(day.datetime);
-        let windowDay =dayObj.toLocaleDateString("en-US", { weekday: "long" });
-        let windowDayIndex=windowDaysArr.indexOf(windowDay);
-
-        if (windowDaysArr.includes(windowDay)) {
-            let hourArr = day.hours.filter((item, index) => {
-                let itemHour = parseInt(item.datetime.split(":")[0]);
-                let itemMin = parseInt(item.datetime.split(":")[1]);
-                if ((windowArr[windowDayIndex].startWindowHour <= itemHour)
-                    && (windowArr[windowDayIndex].endWindowHour >= itemHour))
-                    return{};
-            }).map((item, index) => {
-                let itemHour = parseInt(item.datetime.split(":")[0]);
-                let itemMin = parseInt(item.datetime.split(":")[1]);
-                let rainStatus = Boolean(item.precipprob>40);
-                return {
-                    datetime: item.datetime,
-                    hour: itemHour,
-                    minute: itemMin,
-                    precipprob: item.precipprob,
-                    precip: item.precip,
-                    temp: item.temp,
-                    rain: rainStatus
-                }
-
-            })
-            // console.log(hourArr);
-            // console.log(windowDay);
-            // console.log(windowDaysArr);
-            // console.log(windowArr[windowDaysArr.indexOf(windowDay)]);
-
-            windowArr[windowDaysArr.indexOf(windowDay)].dataBomb = {
-                day: dayObj.toLocaleDateString("en-US", { weekday: "long" }),
-                hours: hourArr,
-                date: day.datetime,
-                month: dayObj.toLocaleDateString("en-US", { month: "long" }),
-                year: dayObj.toLocaleDateString("en-US", { year: "numeric" })
-            }
-        }
-    });
-
-
-
-
-    return windowArr;
-}
-
-const weatherDataFilter2=(weatherObj,windowArr)=>{
     let weatherRequiredObj={};
     let weatherDayObj={};
     let windowDaysArr=windowArr.map((item,index)=>{
         return item.startWindowDay;
     })
-    // console.log(windowArr);
-    // console.log(windowDaysArr);
-
-    
+   
 
     weatherObj.days.slice(0, 7).forEach((day, dayIndex) => {
         let dayObj = new Date(day.datetime);
@@ -112,10 +54,7 @@ const weatherDataFilter2=(weatherObj,windowArr)=>{
                 }
 
             })
-            // console.log(hourArr);
-            // console.log(windowDay);
-            // console.log(windowDaysArr);
-            // console.log(windowArr[windowDaysArr.indexOf(windowDay)]);
+
             weatherDayObj[dayObj.toLocaleDateString("en-US", { weekday: "long" })] =
             {
                 day: day.datetime,
@@ -132,7 +71,7 @@ const weatherDataFilter2=(weatherObj,windowArr)=>{
         }
         
     });
-    // console.log(weatherDayObj);
+
     finalWindowArr = windowArr.map((item, index) => {
         item.date = weatherDayObj[item.startWindowDay].day;
         item.windowHours = weatherRequiredObj[item.startWindowDay].hours.filter((weatherItem) => {
@@ -141,7 +80,6 @@ const weatherDataFilter2=(weatherObj,windowArr)=>{
             }
         });
 
-        // console.log(item.windowHours);
 
         return item;
     })
@@ -156,7 +94,6 @@ const weatherDataFilter2=(weatherObj,windowArr)=>{
 
 
 
-    // console.log(weatherRequiredObj);
 
     return finalWindowArr;
 }
