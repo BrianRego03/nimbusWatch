b0c2a3779514f9f9cd0d998614016ff016954adc
brianrego03 <brian.rego03@gmail.com>
2025-08-14 11:27:15 +0530

    Remove status from responses

--- config/authMiddleware.js
+++ config/authMiddleware.js
@@ -8,11 +8,11 @@ const authMiddleware=async(req,res,next)=>{
     try{
         const token=req.cookies.token;
         if(!token){
-            return res.status(401).json({message:'unauthorized:no token'});
+            return res.json({message:'unauthorized:no token'});
         }
         const isBlacklisted = await redisClient.get(`bl:${token}`);
         if(isBlacklisted){
-            return res.status(403).json({message:'Token is blacklisted'});
+            return res.json({message:'Token is blacklisted'});
         }
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
         const userId=decoded.id;
@@ -25,13 +25,13 @@ const authMiddleware=async(req,res,next)=>{
 
         user=await findUserByID(userId);
         if(!user){
-            return res.status(404).json({message:"User not found"});
+            return res.json({message:"User not found"});
         }
         await redisClient.set(`user:${userId}`,JSON.stringify(user),{EX:86400});
         req.user=user;
         next();
     }catch(err){
-        return res.status(403).json({message:"Invalid token"});
+        return res.json({message:"Invalid token"});
     }
 };
 
