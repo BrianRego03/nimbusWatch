1a9d12df03b0611a0db2465ff5b1bf30a27ce8bc
brianrego03 <brian.rego03@gmail.com>
2025-09-10 10:36:03 +0530

    Fix redis url

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,5 +1,5 @@
 const redis=require("redis");
-const client= redis.createClient();
+const client= redis.createClient({url: process.env.REDIS_URL || 'redis://redis:6379/0'});
 
 client.on('error',(err)=>console.error('Redis error:',err));
 client.connect();
